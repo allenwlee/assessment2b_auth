e1fe7f1e3d654db6fbe0f8315a8d3f0ab5515005
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-05-23 10:33:31 -0700

    done

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
 
   // send an HTTP DELETE request for the sign-out link
-  $('a#sign-out').on("click", function (e) {
+  $('#sign-out').on("click", function (e) {
     e.preventDefault();
+    console.log("hey");
     var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
     request.done(function () { window.location = "/"; });
   });
